9f0596f9958d0ec395cc7820d1f05fb913a9697a
Alex Ramos <atramos@gmail.com>
2020-09-02 18:16:38 -0700

    adding class-based jdklogging configuration

---
+++ jaws-servlet-logger/src/main/java/org/jaws/logging/CloudwatchLogConfig.java
@@ -0,0 +1,27 @@
+package org.jaws.logging;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Instant;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class CloudwatchLogConfig {
+
+	public CloudwatchLogConfig() {
+		
+		LogManager lm = LogManager.getLogManager();
+		
+		try(InputStream is = getClass().getClassLoader().getResourceAsStream("jaws-cloudwatch-logging.properties")) {
+			lm.readConfiguration(is);
+			Logger.getLogger("").getHandlers()[0].setFormatter(new SimpleFormatter());
+			System.err.println(Instant.now() + " *** LOGGER INIT OK ***");
+		} catch (SecurityException | IOException e) {
+			System.err.println(Instant.now() + " *** FAILED TO INITIALIZE CLOUDWATCH LOGGING ***");
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
