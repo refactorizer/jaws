00da43f18dc62ab94b8009caca46319ef3bc5ba7
Alex Ramos <atramos@gmail.com>
2021-01-08 20:27:07 -0800

    remove commons-logging and jackson-datatype-jsr310 dependency

--- jaws-s3-json-db/src/main/java/com/github/atramos/jaws/AwsS3Template.java
+++ jaws-s3-json-db/src/main/java/com/github/atramos/jaws/AwsS3Template.java
@@ -63,7 +63,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class AwsS3Template {
 
@@ -75,16 +74,10 @@ public class AwsS3Template {
 
 	private String bucket;
 
+        private ObjectMapper objectMapper;
+
 	public String getBucket() {
 		return bucket;
-	}
-
-	public ObjectMapper objectMapper = new ObjectMapper();
-	{
-		objectMapper.registerModule(new JavaTimeModule());
-		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-		objectMapper
-				.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
 	}
 
 	public String gzipRead(String path, boolean skipStaleCheck) {
@@ -652,12 +645,14 @@ public class AwsS3Template {
 		return s3client.doesObjectExist(bucket, path);
 	}
 
-	public AwsS3Template(AWSCredentialsProvider awsCredentials) {
+        public AwsS3Template(ObjectMapper objectMapper,
+                AWSCredentialsProvider awsCredentials) {
 		this.awsCredentials = awsCredentials;
+                this.objectMapper = objectMapper;
 	}
 
-	public AwsS3Template() {
-		this(new DefaultAWSCredentialsProviderChain());
+        public AwsS3Template(ObjectMapper objectMapper) {
+            this(objectMapper, new DefaultAWSCredentialsProviderChain());
 	}
 
 	/**
